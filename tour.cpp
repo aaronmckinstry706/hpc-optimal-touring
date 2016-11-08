781edc85a74ad146ad628d8805fd3af154775356
aaron <aaronmckinstry706@gmail.com>
2016-11-08 16:41:13 -0500

    generated new test files; added timer to tour.cpp; optimized timer

--- tour.cpp
+++ tour.cpp
@@ -14,25 +14,31 @@
 #include <unistd.h>
 #include <algorithm>
 
+#define CHECK_INTERVAL 100000
+#define TIME_ALLOWED 120
+
+bool stopEarly = false;//determines when whole program should stop
+
 template <typename S, typename T>
 std::ostream& operator<<(std::ostream& os, const std::pair<S,T>& p) {
-    return os << "(" << p.first << "," << p.second << ")";
+	return os << "(" << p.first << "," << p.second << ")";
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
-    std::string s;
-    std::ostringstream oss(s);
-    for (int i = 0; i < v.size(); ++i) {
-        oss << v[i] << ' ';
-    }
-    s = oss.str();
-    s.pop_back();
-    os << s;
-    return os;
+	std::string s;
+	std::ostringstream oss(s);
+	for (int i = 0; i < v.size(); ++i) {
+		oss << v[i] << ' ';
+	}
+	s = oss.str();
+	s.pop_back();
+	os << s;
+	return os;
 }
 
-#define CLOSEST 50
+int CLOSEST = 20;
+time_t programRealStartTime;
 
 int MAX_X = -1;
 int MAX_Y = -1;
@@ -47,12 +53,12 @@ struct site {
 };
 
 std::ostream& operator<<(std::ostream& os,const struct site& site) {
-    return os << "(n=" << site.n
-              << ",x=" << site.x
-              << ",y=" << site.y
-              << ",dur=" << site.duration
-              << ",val=" << site.value
-              << ")";
+	return os << "(n=" << site.n
+			  << ",x=" << site.x
+			  << ",y=" << site.y
+			  << ",dur=" << site.duration
+			  << ",val=" << site.value
+			  << ")";
 }
 
 struct times {
@@ -60,16 +66,16 @@ struct times {
 	int day;
 	int open;
 	int close;
-    times() {n = -1;}
-    bool invalid() {return n < 0;}
+	times() {n = -1;}
+	bool invalid() {return n < 0;}
 };
 
 std::ostream& operator<<(std::ostream& os,const struct times& times) {
-    return os << "(n=" << times.n
-              << ",day=" << times.day
-              << ",open=" << times.open
-              << ",close=" << times.close
-              << ")";
+	return os << "(n=" << times.n
+			  << ",day=" << times.day
+			  << ",open=" << times.open
+			  << ",close=" << times.close
+			  << ")";
 }
 
 struct cluster {
@@ -78,9 +84,9 @@ struct cluster {
 };
 
 std::ostream& operator<<(std::ostream& os, const struct cluster& c) {
-    return os << "(x=" << c.x
-              << ",y=" << c.y
-              << ")";
+	return os << "(x=" << c.x
+			  << ",y=" << c.y
+			  << ")";
 }
 
 double distance(struct cluster c, struct site s) {
@@ -207,13 +213,13 @@ std::vector<struct cluster> get_cluster_centers(int num_clusters, std::vector<st
 
 std::vector<struct site> get_nearby_sites_to_cluster(struct cluster c, std::vector<struct site> sites) {
 
-    struct site_has_id {
-        int id;
-        site_has_id(int i) : id(i) {}
-        bool operator()(struct site s) {
-            return s.n == id;
-        }
-    };
+	struct site_has_id {
+		int id;
+		site_has_id(int i) : id(i) {}
+		bool operator()(struct site s) {
+			return s.n == id;
+		}
+	};
 
 	std::vector<std::pair<double, int> > site_distances;
 	for (auto const& s : sites) {
@@ -222,81 +228,91 @@ std::vector<struct site> get_nearby_sites_to_cluster(struct cluster c, std::vect
 		site_distances.push_back(p);
 	}
 	
-	// TODO: sort site_distances based on distance (first part of pair), return closest CLOSEST
-    std::vector<struct site> closest;
-    std::sort(site_distances.begin(), site_distances.end());
-    int count = 0;
-    for (auto const& p : site_distances) {
-        auto it = std::find_if(sites.begin(), sites.end(), site_has_id(p.second));
-        closest.push_back(*it);
-        if (++count == CLOSEST)
-            break;
-    }
-    
+	std::vector<struct site> closest;
+	std::sort(site_distances.begin(), site_distances.end());
+	int count = 0;
+	for (auto const& p : site_distances) {
+		auto it = std::find_if(sites.begin(), sites.end(), site_has_id(p.second));
+		closest.push_back(*it);
+		if (++count == CLOSEST)
+			break;
+	}
+	
 	return closest;
 }
 
 int time_finished_visiting(struct site site,
-                           struct times openHours,
-                           std::pair<int,int> curPos,
-                           int curTime) {
-    // std::cout << "going to site " << site
-              // << ",\n\thours " << openHours
-              // << ",\n\tfrom position " << curPos
-              // << ",\n\tat time " << curTime << ":" << std::endl;
-    int travelTime;
-    if (curTime == 0)
-        travelTime = openHours.open*60;
-    else {
-        travelTime = abs(site.x - curPos.first) + abs(site.y - curPos.second);
-        travelTime = std::max(openHours.open*60-curTime,travelTime);
-    }
-    int visitTime = site.duration;
-    int finishedTime = curTime + travelTime + visitTime;
-    // std::cout << "\t\tfinished time is " << finishedTime << std::endl;
-    if (finishedTime > openHours.close*60)
-        return -1;
-    else
-        return finishedTime;
+						   struct times openHours,
+						   std::pair<int,int> curPos,
+						   int curTime) {
+	int travelTime;
+	if (curTime == 0)
+		travelTime = openHours.open*60;
+	else {
+		travelTime = abs(site.x - curPos.first) + abs(site.y - curPos.second);
+		travelTime = std::max(openHours.open*60-curTime,travelTime);
+	}
+	int visitTime = site.duration;
+	int finishedTime = curTime + travelTime + visitTime;
+	if (finishedTime > openHours.close*60)
+		return -1;
+	else
+		return finishedTime;
 }
 
+int tick = 0;
+
 std::pair<double,std::vector<int> > optimalPath(
-        std::vector<struct site>& sites,
-        std::vector<struct times>& times,
-        std::vector<bool>& used,
-        int curTime = 0,
-        std::pair<int,int> curPos = std::pair<int,int>(0,0)) {
-    
-    // std::cout << curPos << ' ' << curTime << std::endl;
-    
-    double bestValue = 0;
-    std::vector<int> bestPath;
-    
-    for (int i = 0; i < sites.size(); ++i) {
-        if (used[i] || times[i].invalid()) continue;
-        
-        int nextTime = time_finished_visiting(sites[i],times[i],curPos,curTime);
-        std::pair<int,int> nextPosition = std::pair<int,int>(sites[i].x,sites[i].y);
-        if (nextTime != -1) {
-            
-            used[i] = true;
-            std::pair<double,std::vector<int> > value_remainingPath =
-                optimalPath(sites, times, used, nextTime, nextPosition);
-            used[i] = false;
-            
-            double totalValue = value_remainingPath.first + sites[i].value;
-            std::vector<int>& path = value_remainingPath.second;
-            
-            if (totalValue > bestValue) {
-                bestPath.clear();
-                bestPath.push_back(i);
-                bestPath.insert(bestPath.end(),path.begin(),path.end());
-                bestValue = totalValue;
-            }
-        }
-    }
-    
-    return std::pair<double,std::vector<int> >(bestValue,bestPath);
+					std::vector<struct site>& sites,
+					std::vector<struct times>& times,
+					std::vector<bool>& used,
+					int curTime = 0,
+					std::pair<int,int> curPos = std::pair<int,int>(0,0),
+					int depth = 0) {
+	
+	double bestValue = 0;
+	std::vector<int> bestPath;
+	
+	if (!stopEarly) {
+	
+	bool foundChild = false;
+	for (int i = 0; i < sites.size(); ++i) {
+		if (used[i] || times[i].invalid()) continue;
+		
+		int nextTime = time_finished_visiting(sites[i],times[i],curPos,curTime);
+		std::pair<int,int> nextPosition = std::pair<int,int>(sites[i].x,sites[i].y);
+		if (nextTime != -1) {
+			foundChild = true;
+			
+			used[i] = true;
+			std::pair<double,std::vector<int> > value_remainingPath =
+				optimalPath(sites, times, used, nextTime, nextPosition, depth+1);
+			used[i] = false;
+			
+			double totalValue = value_remainingPath.first + sites[i].value;
+			std::vector<int>& path = value_remainingPath.second;
+			
+			if (totalValue > bestValue) {
+				bestPath.clear();
+				bestPath.push_back(i);
+				bestPath.insert(bestPath.end(),path.begin(),path.end());
+				bestValue = totalValue;
+			}
+			if (stopEarly) break;
+		}
+	}
+	if (!foundChild) {
+		tick = (tick+1)%CHECK_INTERVAL;
+		if (tick%CHECK_INTERVAL == 0) {
+			//check time remaining
+			if (time(0) - programRealStartTime >= TIME_ALLOWED - 5)
+				stopEarly = true;
+		}
+	}
+	
+	}//end stopEarly if-statement
+	
+	return std::pair<double,std::vector<int> >(bestValue,bestPath);
 }
 
 // function will consider any site close to cluster_order[i][k] for day k
@@ -308,99 +324,91 @@ std::pair<double,std::vector<int> > get_value_around_cluster_for_day(
 		struct cluster c,
 		int day,
 		std::vector<bool>& globalUsed) {
-    
-    struct times_has_day_id {
-        int day;
-        int id;
-        times_has_day_id(int d, int i) : day(d),id(i) {}
-        bool operator()(struct times t) {return t.n == id && t.day == day;}
-    };
-    
-    // std::cout << "times to search: " << std::endl;
-    // std::cout << times << std::endl;
-
+	
+	struct times_has_day_id {
+		int day;
+		int id;
+		times_has_day_id(int d, int i) : day(d),id(i) {}
+		bool operator()(struct times t) {return t.n == id && t.day == day;}
+	};
+	
 	double value = 0.0;
 	std::vector<struct site> nearby = get_nearby_sites_to_cluster(c, sites);
-    std::vector<struct times> nearbyTimes;
-    for (auto const& s : nearby) {
-        // std::cout << "search for time corresponding to " << s << ", day " << day+1 << "..." << std::endl;
-        auto it = std::find_if(times.begin(),times.end(),times_has_day_id(day+1,s.n));
-        if (it != times.end()) {
-            nearbyTimes.push_back(*it);
-            // std::cout << "found time " << *it << std::endl;
-        }
-    }
-    // std::cout << "nearby:" << std::endl << nearby << std::endl
-              // << "nearbyTimes:" << std::endl << nearbyTimes << std::endl;
-    
-    std::vector<bool> used(nearby.size(),false);
-    for (int i = 0; i < used.size(); ++i)
-        if (globalUsed[nearby[i].n])
-            used[i] = true;
-    
-    std::pair<double,std::vector<int> > bestPath = optimalPath(nearby,nearbyTimes,used);
-    
-    //switch back to global id-indexing (site.n), rather than indexing into nearby[]
-    for (int& i : bestPath.second) {
-        i = nearby[i].n;
-        globalUsed[i] = true;
-    }
+	std::vector<struct times> nearbyTimes;
+	for (auto const& s : nearby) {
+		auto it = std::find_if(times.begin(),times.end(),times_has_day_id(day+1,s.n));
+		if (it != times.end()) {
+			nearbyTimes.push_back(*it);
+		}
+	}
+	
+	std::vector<bool> used(nearby.size(),false);
+	for (int i = 0; i < used.size(); ++i)
+		if (globalUsed[nearby[i].n])
+			used[i] = true;
+	
+	std::pair<double,std::vector<int> > bestPath = optimalPath(nearby,nearbyTimes,used);
+	
+	//switch back to global id-indexing (site.n), rather than indexing into nearby[]
+	for (int& i : bestPath.second) {
+		i = nearby[i].n;
+		globalUsed[i] = true;
+	}
 	
 	return bestPath;
 }
 
-int main(int argc, char *argv[]) {
+int myrandom(int i) { return std::rand()%i; }
 
+int main(int argc, char *argv[]) {
+	std::srand( unsigned ( std::time(0) ) );
+	
+	programRealStartTime = time(0);
+	
 	std::vector<struct site> sites;
 	std::vector<struct times> site_times;
 	int days = parse_input(argv[1], &sites, &site_times);
-    
-    // std::cout << "sites: " << std::endl << sites << std::endl;
-    // std::cout << "site times: " << std::endl << site_times << std::endl;
-    
-    struct lexical_cluster_comparison {
-        lexical_cluster_comparison() {}
-        bool operator()(const struct cluster& c1, const struct cluster& c2) {
-            return c1.x < c2.x || (c1.x == c2.x && c1.y < c2.y);
-        }
-    };
+	
+	struct lexical_cluster_comparison {
+		lexical_cluster_comparison() {}
+		bool operator()(const struct cluster& c1, const struct cluster& c2) {
+			return c1.x < c2.x || (c1.x == c2.x && c1.y < c2.y);
+		}
+	};
 	std::vector<struct cluster> clusters = get_cluster_centers(days, sites);
-    std::sort(clusters.begin(),clusters.end(),lexical_cluster_comparison());
-    
-    // std::cout << "clusters: " << std::endl << clusters << std::endl;
-    
+	std::sort(clusters.begin(),clusters.end(),lexical_cluster_comparison());
+	
 	std::vector<int> best_tour;
 	double best_value = 0;
+	
 
-	// TODO: add in timer so it finishes within 2min
 	do {
-        
-        std::vector<bool> globalUsed(200,false);
-        std::vector<int> globalBestPath;
-        
+		std::vector<bool> globalUsed(200,false);
+		std::vector<int> globalBestPath;
+		
 		double value = 0;
 		for (int k = 0; k < days; k++) {
 			std::pair<double, std::vector<int> > bestPathInCluster =
-                get_value_around_cluster_for_day(
-                    sites, site_times, clusters[k], k, globalUsed);
-            
-            value += bestPathInCluster.first;
-            globalBestPath.insert(globalBestPath.end(),
-                                  bestPathInCluster.second.begin(),
-                                  bestPathInCluster.second.end());
+				get_value_around_cluster_for_day(
+					sites, site_times, clusters[k], k, globalUsed);
+			
+			value += bestPathInCluster.first;
+			globalBestPath.insert(globalBestPath.end(),
+						  bestPathInCluster.second.begin(),
+						  bestPathInCluster.second.end());
+			
+			if (stopEarly) break;
 		}
 		if (value > best_value) {
-            std::cout << best_value << std::endl;
 			best_value = value;
 			best_tour = globalBestPath;
 		}
-        if (clusters.size() == 1)
-            break;
-	} while (std::next_permutation(
-              clusters.begin(),clusters.end(), lexical_cluster_comparison()));
-    
+		if (stopEarly) break;
+		
+		std::random_shuffle(clusters.begin(),clusters.end(), myrandom);
+	} while (true);
+	
 	std::cout << best_tour << std::endl;
-    std::cout << best_value << std::endl;
 
 	return 0;
 }
